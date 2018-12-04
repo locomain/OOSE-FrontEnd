4ebbded126dc3b2d6ec0acc3e50214a06cad2f1b
Angelo <locomain12@gmail.com>
2018-12-04 12:51:04 +0100

    braw added

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,14 @@
-import '@locomain/brawts'
+///<reference types="@locomain/brawts" />
 
-//TODO add types to packagejson
-//TODO add exports
-//TODO braw
-/*@component({
+import {component} from '@locomain/brawts'
+
+@component({
     tag:"app-root",
     view:`<h1>test</h1>`
-})*/
+})
 class App extends Component {
-/*
-    private static instance: App;
 
-    public static getInstance(){
-        if(!this.instance)this.instance = new App();
-        return this.instance;
+    onRender(): void{
+        console.log("rendering app");
     }
-
-    private constructor(){
-
-    }*/
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode:'development',
     entry: './src/app.ts',
     module: {
         rules: [
