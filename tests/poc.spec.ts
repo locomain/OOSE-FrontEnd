8fcd77c0d06f5fb0f07c0363833f891446e79c66
Angelo <locomain12@gmail.com>
2019-01-21 21:34:19 +0100

    waegwg

--- tests/poc.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import 'mocha';
-
-describe('Proof of concept',()=>{
-    it(`Should return-> Works~!`,()=>{
-       expect(`Works~!`).to.equal(`Works~!`);
-    });
-});
---
+++ tests/utils.test.ts
@@ -0,0 +1,27 @@
+//@ts-ignore
+import { expect } from 'chai';
+import 'mocha';
+import {Utils} from '../app/utils/utils';
+
+describe('Property keys to lowercase',()=>{
+    it(`Should return-> { data : 1 }`,()=>{
+        const data = {DATA:1};
+        Utils.keysToLowerCase(data);
+        expect(JSON.stringify(data)).to.equal(JSON.stringify({data:1}));
+    });
+});
+
+
+describe('Convert database date to a readable date',()=>{
+    it('Should return -> 01-01-2020',()=>{
+        const result = Utils.dateToReadableDate("2020-01-01 00:00:00");
+        expect(result).to.equal("01-01-2020");
+    });
+});
+
+
+describe('Should check if a string has a uppercase char',()=>{
+   it('Should return -> true',()=>{
+     expect(true).to.equal(true);
+   });
+});
